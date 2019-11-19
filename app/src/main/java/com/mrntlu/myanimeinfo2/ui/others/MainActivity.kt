0ffead73b3e45685d30c37f711102626fffa54c3
Burak Fidan <97burakfidan97@gmail.com>
2019-11-19 17:47:41 +0300

    Navigation user profile header set. User Profile UI & navigation setup. Global destinations set.

--- app/src/main/java/com/mrntlu/myanimeinfo2/ui/others/MainActivity.kt
+++ app/src/main/java/com/mrntlu/myanimeinfo2/ui/others/MainActivity.kt
@@ -7,27 +7,45 @@ import android.view.WindowManager
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.core.os.bundleOf
 import androidx.core.view.GravityCompat
+import androidx.core.view.GravityCompat.*
 import androidx.core.view.isVisible
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import com.google.android.gms.ads.AdListener
 import com.google.android.gms.ads.AdRequest
 import com.google.android.gms.ads.MobileAds
 import com.mrntlu.myanimeinfo2.R
+import com.mrntlu.myanimeinfo2.interfaces.CoroutinesErrorHandler
 import com.mrntlu.myanimeinfo2.models.DataType
+import com.mrntlu.myanimeinfo2.models.DataType.*
+import com.mrntlu.myanimeinfo2.models.UserProfileResponse
 import com.mrntlu.myanimeinfo2.utils.*
+import com.mrntlu.myanimeinfo2.viewmodels.CommonViewModel
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.nav_no_user_header.view.*
+import kotlinx.android.synthetic.main.nav_no_user_header.view.malUserText
+import kotlinx.android.synthetic.main.nav_user_header.view.*
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var navController: NavController
+    private lateinit var navHeader:View
+    private lateinit var commonViewModel: CommonViewModel
+    private var malUser:UserProfileResponse?=null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val navHostFragment=nav_host_fragment as NavHostFragment
         navController=navHostFragment.navController
+        commonViewModel=ViewModelProviders.of(this).get(CommonViewModel::class.java)
 
         navController.addOnDestinationChangedListener { _, destination, arguments ->
             toolbar.title=when(destination.id){
@@ -106,28 +124,76 @@ class MainActivity : AppCompatActivity() {
             when(it.itemId){
                 R.id.main_page_menu->navController.navigate(R.id.action_global_main)
                 R.id.anime_schedule->navController.navigate(R.id.action_global_scheduleAnime)
-                R.id.top_anime_list->navController.navigate(R.id.action_global_topList,bundleOf("data_type" to DataType.ANIME.code))
+                R.id.top_anime_list->navController.navigate(R.id.action_global_topList,bundleOf("data_type" to ANIME.code))
                 R.id.anime_by_season->navController.navigate(R.id.action_global_animeSeason)
-                R.id.top_manga_list->navController.navigate(R.id.action_global_topList, bundleOf("data_type" to DataType.MANGA.code))
-                R.id.anime_search->navController.navigate(R.id.action_global_search, bundleOf("data_type" to DataType.ANIME.code))
-                R.id.manga_search->navController.navigate(R.id.action_global_search, bundleOf("data_type" to DataType.MANGA.code))
+                R.id.top_manga_list->navController.navigate(R.id.action_global_topList, bundleOf("data_type" to MANGA.code))
+                R.id.anime_search->navController.navigate(R.id.action_global_search, bundleOf("data_type" to ANIME.code))
+                R.id.manga_search->navController.navigate(R.id.action_global_search, bundleOf("data_type" to MANGA.code))
                 R.id.search_user->navController.navigate(R.id.action_global_userSearch)
             }
 
-            drawerLayout.closeDrawer(GravityCompat.START)
+            drawerLayout.closeDrawer(START)
             true
         }
+        setHeader()
 
         val toggle=ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.open,R.string.close)
         toggle.setToolbarNavigationClickListener {
-            if (drawerLayout.isDrawerVisible(GravityCompat.START)){
-                drawerLayout.closeDrawer(GravityCompat.START)
+            if (drawerLayout.isDrawerVisible(START)){
+                drawerLayout.closeDrawer(START)
             }else{
-                drawerLayout.openDrawer(GravityCompat.START)
+                drawerLayout.openDrawer(START)
             }
         }
         drawerLayout.addDrawerListener(toggle)
         toggle.syncState()
+    }
+
+    private fun setHeader() {
+        if (malUser!=null){
+            if(::navHeader.isInitialized) navigationView.removeHeaderView(navHeader)
+            navHeader=navigationView.inflateHeaderView(R.layout.nav_user_header)
+            navHeader.malUserText.text=malUser!!.username
+            if(malUser!!.image_url != null) navHeader.malUserImage.loadWithGlide(malUser!!.image_url!!,navHeader.malUserProgress)
+            else navHeader.malUserImage.setImageDrawable(resources.getDrawable(R.drawable.ic_person_black_24dp,theme))
+
+            setHeaderListeners(navHeader)
+        }else{
+            navHeader=navigationView.inflateHeaderView(R.layout.nav_no_user_header)
+            navHeader.malUserAddButton.setOnClickListener {
+                commonViewModel.getUserProfile("mrntlu",object:CoroutinesErrorHandler{
+                    override fun onError(message: String) {
+                        GlobalScope.launch(Dispatchers.Main){
+
+                        }
+                    }
+                }).observe(this, Observer {
+                    malUser=it
+                    setHeader()
+                })
+            }
+        }
+
+    }
+
+    private fun setHeaderListeners(navHeader: View) {
+        navHeader.malUserProfileButton.setOnClickListener {
+            val bundle = bundleOf("username" to malUser!!.username)
+            navController.navigate(R.id.action_global_userProfile,bundle)
+            drawerLayout.closeDrawer(START)
+        }
+
+        navHeader.malUserAnimeButton.setOnClickListener {
+            val bundle= bundleOf("username" to malUser!!.username, "data_type" to ANIME.code)
+            navController.navigate(R.id.action_global_userList,bundle)
+            drawerLayout.closeDrawer(START)
+        }
+
+        navHeader.malUserMangaButton.setOnClickListener {
+            val bundle= bundleOf("username" to malUser!!.username, "data_type" to MANGA.code)
+            navController.navigate(R.id.action_global_userList,bundle)
+            drawerLayout.closeDrawer(START)
+        }
     }
 
     override fun onDestroy() {
